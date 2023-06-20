33f2629edeea3f32236e573e92d0ece098918d3f
Kawasem1 <89655907+Kawasem1@users.noreply.github.com>
2023-06-20 14:16:15 +0900

    マークシート読み取り数増加、答え合わせ実装

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import pyocr
 #環境変数「PATH」にTesseract-OCRのパスを設定。
 path='C:\\Program Files\\Tesseract-OCR\\'
 os.environ['PATH'] = os.environ['PATH'] + path
+#回答の数、配列初期化、模範解答
+e_cnt = 10
 ans = []
+ans_t=[3,4,3,4,3,4,5,5,6,7]
 #pyocrにTesseractを指定する。
-pyocr.tesseract.TESSERACT_CMD = r'C:\\Users\prf103\Desktop\python\tesseract\tesseract.exe'
+pyocr.tesseract.TESSERACT_CMD = r'.\tesseract\tesseract.exe'
 tools = pyocr.get_available_tools()
 tool = tools[0]
 
@@ -16,15 +19,22 @@ k = 50
 b = 1
 c = 6
 #画像
-for a in range(4):
-    img = Image.open('C:\\Users\prf103\Desktop\python\example4.png')
+for a in range(e_cnt):
+    img = Image.open('.\example4.png')
     img_dtl=img.crop((k*a,0,k*b,50))
 #画像の文字を抽出
     builder = pyocr.builders.TextBuilder(tesseract_layout=6)
     text = tool.image_to_string(img_dtl, lang="eng", builder=builder)
 #"|"を消去しintへ変換
     text = text.replace("|","")
-    int(text)
+    text =int(text)
     ans.append(text)
     b+=1
+#答え合わせ
+d = 0
+for d in range(e_cnt):
+    if ans[d] == ans_t[d]:
+        print(f'Q{d+1} 正解!')
+    else:
+        print(f'Q{d+1} 不正解 => A.{ans_t[d]}')
 print(ans)
