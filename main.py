671c717de85c1ede4de94a7d2166154f828e9a19
prf103 <prf103@SS042PRF103>
2023-07-19 14:06:45 +0900

    ほぼ完成

---
+++ main.py
@@ -0,0 +1,23 @@
+def yomitori(inputPath,outputPath,name):
+	import os
+	from PIL import Image
+	import pyocr
+	path='C:\\Program Files\\Tesseract-OCR\\'
+	os.environ['PATH'] = os.environ['PATH'] + path
+	#回答の数、配列初期化、模範解答
+	ans = []
+	#pyocrにTesseractを指定する。
+	pyocr.tesseract.TESSERACT_CMD = r'..\tesseract\tesseract.exe'
+	tools = pyocr.get_available_tools()
+	tool = tools[0]
+	img = Image.open(inputPath)
+	#画像の文字を抽出
+	builder = pyocr.builders.TextBuilder(tesseract_layout=6)
+	text = tool.image_to_string(img, lang="jpn", builder=builder)
+	ans.append(text)
+	path = os.path.join(outputPath,name) +'.txt'
+	f = open(path, 'w')
+	f.writelines(ans)
+	f.close()
+	from tkinter import messagebox
+	messagebox.showinfo('読み取る君','読み取り完了！出力先:' + outputPath)
--- window.py
+++ window.py
@@ -6,26 +6,57 @@ root = tkinter.Tk()
 root.title(u"読み取る君(beta)")
 # windnow size w*h
 root.geometry("480x250")
-static1 = tkinter.Label(text=u'読み取る画像のパスを入力してください',font=('',12))
+#ファイル読み取り処理
+static1 = tkinter.Label(text=u'読み取る画像のパスを入力してください')
 static1.place(x=10,y=10)
 editbox = tkinter.Entry(width=50)
 editbox.place(x=10,y=30)
 
-def pathRead(event):
-    path = editbox.get()
-    print(path)
+def Read(event):
+    from tkinter import messagebox
+    inputPath = editbox.get()
+    if len(inputPath) == 0 :
+        messagebox.showerror('読み取る君', '読み取りファイルパスが未入力です。')
+    outputPath = editbox2.get()
+    if len(outputPath) == 0 :
+        messagebox.showerror('読み取る君', '出力先ファイルパスが未入力です。')
+    print(inputPath,outputPath)
+    import os
+    name = os.path.splitext(os.path.basename(inputPath))[0]
+    import main
+    main.yomitori(inputPath,outputPath,name)
     
 button = tkinter.Button(text=u'読み取る')
 button.place(x=210,y=220)
-button.bind("<Button-1>",pathRead)
+button.bind("<Button-1>",Read)
 
-filesansyo = tkinter.Button(text=u'ファイル選択')
+filesansyo = tkinter.Button(text=u'参照')
 filesansyo.place(x=320,y=27)
+
 def sansyo(event):
     from tkinter import filedialog
     typ = [('画像ファイル','*.png')]
     fle = filedialog.askopenfilename(filetypes = typ, initialdir = dir)
     editbox.insert(tkinter.END,fle)
 filesansyo.bind("<Button-1>",sansyo)
+
+#出力ファイル読み取り処理
+static2 = tkinter.Label(text=u'出力ファイルの保存先を指定してください。')
+static2.place(x=10,y=50)
+editbox2 = tkinter.Entry(width=50)
+editbox2.place(x=10,y=70)
+outputFlie = tkinter.Button(text=u'参照')
+outputFlie.place(x=320,y=67)
+
+def pathRead2(event):
+    from tkinter import messagebox
+
+    print(outputPath)
+
+def foldersansyo(event):
+    from tkinter import filedialog
+    fld = filedialog.askdirectory(initialdir = dir) 
+    editbox2.insert(tkinter.END,fld)
+outputFlie.bind("<Button-1>",foldersansyo)
 
 root.mainloop()
