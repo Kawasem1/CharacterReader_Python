f2175050c076440697909c9d53f6e4ea31b1355d
Kawasem1 <89655907+Kawasem1@users.noreply.github.com>
2023-09-13 14:50:06 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,23 @@
-import os
-from PIL import Image
-import pyocr
-
-#環境変数「PATH」にTesseract-OCRのパスを設定。
-path='C:\\Program Files\\Tesseract-OCR\\'
-os.environ['PATH'] = os.environ['PATH'] + path
-#回答の数、配列初期化、模範解答
-e_cnt = 10
-ans = []
-ans_t=[3,4,3,4,3,4,5,5,6,7]
-#pyocrにTesseractを指定する。
-pyocr.tesseract.TESSERACT_CMD = r'..\tesseract\tesseract.exe'
-tools = pyocr.get_available_tools()
-tool = tools[0]
-
-#抽出定数
-k = 50
-b = 1
-c = 6
-f = 0
-#画像
-def kaiseki(k,b,c,f):
-    for a in range(e_cnt):
-        img = Image.open('.\example5.png')
-        img_dtl=img.crop((k*a,50*f,k*b,50*(f+1)))
-    #画像の文字を抽出
-        builder = pyocr.builders.TextBuilder(tesseract_layout=6)
-        text = tool.image_to_string(img_dtl, lang="eng", builder=builder)
-    #"|"を消去しintへ変換
-        #text = text.replace("|","")
-        #text =int(text)
-        ans.append(text)
-        b+=1
-    #答え合わせ
-    d = 0
-    for d in range(e_cnt):
-        if ans[d] == ans_t[d]:
-            print(f'Q{d+1} 正解!')
-        else:
-            print(f'Q{d+1} 不正解 => A.{ans_t[d]}')
-    print(ans)
-    f+=1
-rawcnt = 2
-for cnt in range(rawcnt):
-    kaiseki(k,b,c,f)
+def yomitori(inputPath,outputPath,name):
+	import os
+	from PIL import Image
+	import pyocr
+	from tkinter import messagebox
+	path='C:\\Program Files\\Tesseract-OCR\\'
+	os.environ['PATH'] = os.environ['PATH'] + path
+	#回答の数、配列初期化、模範解答
+	ans = []
+	#pyocrにTesseractを指定する。
+	pyocr.tesseract.TESSERACT_CMD = r'tesseract\tesseract.exe'
+	tools = pyocr.get_available_tools()
+	tool = tools[0]
+	img = Image.open(inputPath)
+	#画像の文字を抽出
+	builder = pyocr.builders.TextBuilder(tesseract_layout=6)
+	text = tool.image_to_string(img, lang="jpn", builder=builder)
+	ans.append(text)
+	path = os.path.join(outputPath,name) +'.txt'
+	f = open(path, 'w')
+	f.writelines(ans)
+	f.close()
+	messagebox.showinfo('読み取る君','読み取り完了！出力先:' + outputPath)
---
+++ window.py
@@ -0,0 +1,56 @@
+# import Tkinter lib
+import tkinter
+# create Tkinter instance
+root = tkinter.Tk()
+#window title
+root.title(u"読み取る君(beta)")
+# windnow size w*h
+root.geometry("480x250")
+#ファイル読み取り処理
+static1 = tkinter.Label(text=u'読み取る画像のパスを入力してください')
+static1.place(x=10,y=10)
+editbox = tkinter.Entry(width=50)
+editbox.place(x=10,y=30)
+
+def Read(event):
+    from tkinter import messagebox
+    inputPath = editbox.get()
+    if len(inputPath) == 0 :
+        messagebox.showerror('読み取る君', '読み取りファイルパスが未入力です。')
+    outputPath = editbox2.get()
+    if len(outputPath) == 0 :
+        messagebox.showerror('読み取る君', '出力先ファイルパスが未入力です。')
+    import os
+    name = os.path.splitext(os.path.basename(inputPath))[0]
+    import main
+    main.yomitori(inputPath,outputPath,name)
+    
+button = tkinter.Button(text=u'読み取る')
+button.place(x=210,y=220)
+button.bind("<Button-1>",Read)
+
+filesansyo = tkinter.Button(text=u'参照')
+filesansyo.place(x=320,y=27)
+
+def sansyo(event):
+    from tkinter import filedialog
+    typ = [('画像ファイル','*.png')]
+    fle = filedialog.askopenfilename(filetypes = typ, initialdir = dir)
+    editbox.insert(tkinter.END,fle)
+filesansyo.bind("<Button-1>",sansyo)
+
+#出力ファイル読み取り処理
+static2 = tkinter.Label(text=u'出力ファイルの保存先を指定してください。')
+static2.place(x=10,y=50)
+editbox2 = tkinter.Entry(width=50)
+editbox2.place(x=10,y=70)
+outputFlie = tkinter.Button(text=u'参照')
+outputFlie.place(x=320,y=67)
+
+def foldersansyo(event):
+    from tkinter import filedialog
+    fld = filedialog.askdirectory(initialdir = dir) 
+    editbox2.insert(tkinter.END,fld)
+outputFlie.bind("<Button-1>",foldersansyo)
+
+root.mainloop()
